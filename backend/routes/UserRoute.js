78fc853f2f8579e5626da1e1b6fc61eb1d0ef7fe
Difa <najlanadhifa1103@gmail.com>
2025-05-18 21:57:42 +0700

    update

--- backend/routes/UserRoute.js
+++ backend/routes/UserRoute.js
@@ -4,15 +4,30 @@ import {
     getNotesbyId,
     createNote,
     updateNote,
-    deleteNote
+    deleteNote,
+    login, 
+    logout,
 } from "../controllers/NoteController.js";
+import { verifyToken } from "../middleware/VerifyToken.js";
+import { getAccessToken} from "../controllers/TokenController.js";
 
 const router = express.Router();
 
-router.get("/notes", getNotes);
-router.get("/notes/:id", getNotesbyId);
+// Endpoint buat ngambil access token menggunakan refresh token
+router.get("/token", getAccessToken);
+
+// Endpoint buat login & logout
+router.post("/login", login);
+router.delete("/logout", logout);
+
+// Endpoint CRUD users
+// Kita mau endpoint ini tu restricted,
+// alias user yg mau akses endpoint ini harus login dulu,
+// makanya kita kasih middleware fungsi verifyToken yg udah kita buat sebelumnya.
+router.get("/notes", verifyToken, getNotes);
+router.get("/notes/:id", verifyToken, getNotesbyId);
 router.post("/notes", createNote);
-router.patch("/notes/:id", updateNote);
-router.delete("/notes/:id", deleteNote);
+router.patch("/notes/:id", verifyToken, updateNote);
+router.delete("/notes/:id", verifyToken, deleteNote);
 
 export default router;
