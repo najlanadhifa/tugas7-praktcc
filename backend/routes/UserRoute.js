97e5b26b2426d2b8d1a6378a092a6998ea275ba7
Difa <najlanadhifa1103@gmail.com>
2025-05-20 13:53:01 +0700

    update

--- backend/routes/UserRoute.js
+++ backend/routes/UserRoute.js
@@ -1,13 +1,13 @@
 import express from "express";
 import { getNotes, getNotesbyId, createNote, updateNote, deleteNote } from "../controllers/NoteController.js";
-import { verifyToken } from "../middleware/verifyToken.js";
+import { verifyToken } from "../middleware/VerifyToken.js";
 import { Register, Login, refreshToken, logout } from "../controllers/UserController.js";
 
-const router = express.Router();
+const router = express.Router();    
 
 router.get("/notes", verifyToken, getNotes);
 router.get("/notes/:id", verifyToken, getNotesbyId);
-router.post("/notes", createNote);
+router.post("/notes", verifyToken, createNote);
 router.patch("/notes/:id", verifyToken, updateNote);
 router.delete("/notes/:id", verifyToken, deleteNote);
 
