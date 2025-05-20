8ca24662ed0b1c5ca852c7c204f3f1b24df2db37
Difa <najlanadhifa1103@gmail.com>
2025-05-20 15:45:09 +0700

    update

--- frontend/src/components/EditNote.js
+++ frontend/src/components/EditNote.js
@@ -2,8 +2,10 @@ import React, {useState, useEffect} from "react";
 import axios from "axios";
 import { useNavigate, useParams } from "react-router-dom";
 import { BASE_URL } from "../utils";
+import useAuth from '../auth/useAuth';
 
 const EditNote = () => {
+    const { accessToken, refreshAccessToken } = useAuth();
     const [nama, setNama] = useState("");
     const [judul, setJudul] = useState("");
     const [isi, setIsi] = useState("");
@@ -17,11 +19,15 @@ const EditNote = () => {
     const updateNote = async (e) => {
         e.preventDefault();
         try {
-            await axios.patch(`${BASE_URL}/notes/${id}`,{
-                nama,
-                judul, 
-                isi
-            });
+            await axios.patch(
+                `${BASE_URL}/notes/${id}`,
+                { nama, judul, isi },
+                {
+                    headers: {
+                        Authorization: `Bearer ${accessToken}`
+                    }
+                }
+            );
             navigate("/");
         } catch (error) {
             console.log(error);        
@@ -29,12 +35,37 @@ const EditNote = () => {
     };
 
     const getNotesById = async () =>{
-        const response = await axios.get(`${BASE_URL}/notes/${id}`);
+        try {
+        const response = await axios.get(`${BASE_URL}/notes/${id}`,{
+            headers: {
+                Authorization: `Bearer ${accessToken}`
+            }
+        });
         setNama(response.data.nama);
         setJudul(response.data.judul);
         setIsi(response.data.isi);
-    };
-
+    } catch (error) {
+        if (error.response?.status === 401) {
+            try {
+                await refreshAccessToken(); // refresh token
+                // panggil ulang fungsi ini setelah refresh
+                const response = await axios.get(`${BASE_URL}/users/${id}`, {
+                    headers: {
+                        Authorization: `Bearer ${accessToken}`
+                    }                   
+                });
+                setNama(response.data.nama);
+                setJudul(response.data.judul);
+                setIsi(response.data.isi);
+            } catch (err) {
+                console.error("Gagal refresh token:", err);
+            }
+        } else {
+            console.error("Gagal mengambil data user:", error);
+        }
+    }
+};
+    
   return (
     <div className="columns mt-5 is-centered">
         <div className="columns is-half">
