ce136c681f06689dc20705a4fe4339e78168ab66
Difa <najlanadhifa1103@gmail.com>
2025-05-20 18:30:26 +0700

    update

--- frontend/src/routes/RouterApp.js
+++ frontend/src/routes/RouterApp.js
@@ -1,29 +1,29 @@
-// // import { useState } from "react";
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; // Import untuk routing
-// import Login from "../components/Login"; // Halaman login
-// import Register from "../components/Register";
-// import Notes from "../pages/Notes";
-// import ProtectedRoute from "../pages/ProtectedRoute";
+// import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; // Import untuk routing
+import Login from "../components/Login"; // Halaman login
+import Register from "../components/Register";
+import Notes from "../pages/Notes";
+import ProtectedRoute from "../pages/ProtectedRoute";
 
-// function RouterApp() {
-//   return (
-//     <Router>
-//       <Routes>
-//         {/* Pass setIsAuthenticated as prop to LoginPage */}
-//         <Route path="/" element={<LoginPage />} />
-//         <Route path="/login" element={<LoginPage />} />
-//         <Route path="/register" element={<RegisterPage />} />
-//         <Route
-//           path="/notes"
-//           element={
-//             <ProtectedRoute>
-//               <Notes />
-//             </ProtectedRoute>
-//           }
-//         />
-//       </Routes>
-//     </Router>
-//   );
-// }
+function RouterApp() {
+  return (
+    <Router>
+      <Routes>
+        {/* Pass setIsAuthenticated as prop to LoginPage */}
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route
+          path="/notes"
+          element={
+            <ProtectedRoute>
+              <Notes />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </Router>
+  );
+}
 
-// export default RouterApp;
+export default RouterApp;
